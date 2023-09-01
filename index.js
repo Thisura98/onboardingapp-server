9bf03bb10b9716a17ea1c9ec8f7c856b179aa6b1
Thisura98 <thisura1998@gmail.com>
2023-09-01 23:38:16 +0530

    Complete tasks list API

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const { readFile } = require('fs/promises');
 const app = express();
 const port = 80;
 
 const username = "EMP001";
 const password = "sliit@123"; // bad practice, use hashed / encrypted strings (or tokens) for passwords
-
-function sendJson(req, obj){
-    req.send(JSON.stringify(obj));
-}
 
 app.use(bodyParser.json());
 
@@ -19,16 +17,35 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     const body = req.body;
     if (body['username'] == username && body['password'] == password){
-        sendJson(req, {
+        res.json({
             "success": true,
         });
     }
     else{
-        sendJson(req, {
+        res.json({
             "success": false,
         });
     }
 });
+
+app.get('/tasks', (req, res) => {
+    const filePath = path.join(__dirname, 'tasks.json');
+    readFile(filePath)
+    .then(data => {
+        const obj = {
+            "success": true,
+            "tasks": JSON.parse(data)['tasks']
+        }
+        res.json(obj);
+    })
+    .catch(err => {
+        console.log("/tasks error:", err);
+        res.json({
+            "success": false,
+            "message": "Error returning tasks"
+        });
+    })
+})
 
 app.listen(port, () => {
     console.log("Onboarding App v1.0.0 listening on port 80");
