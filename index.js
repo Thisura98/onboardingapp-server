2a8f3aba6c3af095870346f1d2acca4b2ae378fe
Thisura98 <thisura1998@gmail.com>
2023-09-03 13:52:20 +0530

    Minor update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { readFile } = require('fs/promises');
 const app = express();
 const port = 80;
 
